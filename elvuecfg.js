5347ad52c1a04d338571e929dd1fddbba26a9b07
Atif Aziz <code@raboof.com>
2012-05-07 21:44:32 +0200

    Adding sample configuration script

---
+++ elvuecfg.js
@@ -0,0 +1,7 @@
+﻿/*
+config = {
+    src: 'elmah.axd/download', // location of log download URL
+    title: 'TITLE',            // report title
+    limit: 250                 // limit report to these most recent errors
+};
+*/
